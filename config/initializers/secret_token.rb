e4d84810b7dc5502eaaf1132cc6e7a347d6c0f15
carolfly86 <carolfly86@gmail.com>
2014-05-09 10:57:45 -0400

    init fist app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'c7a3c7ef3090333887b0a8fe2ec64b18443c5020661a85a40260c7b994077cad5d3842d73ad98e8473109d3d80882e747658c3a8677b480eb8c4336f08a0d899'
